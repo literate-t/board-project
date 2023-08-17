28fd17c0bd44db683e4c3195147d82dcdd1cbcc9
literate <gaekoon@gmail.com>
2023-08-17 19:14:15 +0900

    #15 - 게시글 관련 뷰 테스트 작성
    어떤 경로로 뷰를 열어줄지 테스트로 명세를 뽑는다
    
    기존 테스르를 실패한 채 올리면 `gradle.build`도 실패하여 배포 자동화까지 실패할 수 있으므로 아직 구현 중이므로 실패하는 테스트는 테스트 제외처리(@Disabled)

---
+++ src/test/java/com/projectboard/controller/ArticleControllerTest.java
@@ -0,0 +1,67 @@
+package com.projectboard.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Disabled("구현 중")
+    @DisplayName("[view][GET] - 게시글 리스트 (게시판) 페이지 - 정상 호출")
+    @Test
+    public void listViewTest() throws Exception {
+
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles")); // key가 있는지
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[view][GET] - 게시글 상세 페이지 - 정상 호출")
+    @Test
+    public void detailViewTest() throws Exception {
+
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("article")) // key가 있는지
+                .andExpect(model().attributeExists("articleComments")); // key가 있는지
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[view][GET] - 게시글 검색 전용 페이지 - 정상 호출")
+    @Test
+    public void searchViewTest() throws Exception {
+
+        mvc.perform(get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/search"));
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[view][GET] - 게시글 해시태그 검색 페이지 - 정상 호출")
+    @Test
+    public void hashTagSearchView() throws Exception {
+
+        mvc.perform(get("/articles/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/search-hashtag"));
+    }
+}
