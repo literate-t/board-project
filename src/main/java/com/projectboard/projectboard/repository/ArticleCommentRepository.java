b527fc084a9eaaea2a359c53e70280b995dd5d32
literate <gaekoon@gmail.com>
2023-08-17 13:56:50 +0900

    #1 - 패키지 경로를 변경하면서 삭제된 파일
    중복된 패키지 명을 제거하면서 패키지 경로가 변경됐다

--- src/main/java/com/projectboard/projectboard/repository/ArticleCommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.projectboard.projectboard.repository;
-
-import com.projectboard.projectboard.domain.ArticleComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
-}
